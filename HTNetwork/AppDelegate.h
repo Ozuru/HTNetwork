60d464ccbea31d3b52612102cb2bf17522f16692
brant <sjwu1234@gmail.com>
2017-01-16 19:13:13 +0800

    first commit

---
+++ HTNetwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTNetwork
+//
+//  Created by hellotalk on 2017/1/16.
+//  Copyright © 2017年 brant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HTNetwork/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HTNetwork
+//
+//  Created by hellotalk on 2017/1/16.
+//  Copyright © 2017年 brant. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
